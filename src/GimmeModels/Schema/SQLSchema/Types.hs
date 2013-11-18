1924cc5e2add1441ed0eac7f4181f86f6a6d0538
Semyon Novikov <none@none>
2013-11-18 22:10:44 +0600

    Dead simple SQL schema parser

---
+++ src/GimmeModels/Schema/SQLSchema/Types.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module GimmeModels.Schema.SQLSchema.Types
+where
+
+import Control.Applicative
+import qualified Data.Attoparsec.Char8 as P
+import Data.ByteString
+import qualified Data.ByteString.Char8 as C 
+import Data.Char
+
+import qualified GimmeModels.Types as BT
+
+data Table = Table {
+      tableName   :: String 
+    , tableFields :: [Field]
+    } deriving (Show)
+
+tableParser :: P.Parser Table
+tableParser = do
+    P.string "create table "
+    n <- P.takeWhile (not . isSpace)
+    P.skipWhile (isSpace)
+    P.char '(' 
+    fs <- P.manyTill fieldParser (P.string ");")
+    return $ Table (C.unpack n) fs 
+
+data Field = Field {
+      fieldName :: String
+    , fieldType :: String
+    } deriving (Show)
+
+fieldGarbage = \c -> isSpace c || c == ','
+fieldEnd = \c -> c == ',' || c == ')' 
+
+fieldParser :: P.Parser Field
+fieldParser = do
+    P.skipWhile fieldGarbage 
+    n <- P.takeWhile (not . fieldGarbage)
+    P.skipWhile (isSpace)
+    t <- P.takeWhile (not . fieldGarbage) 
+    P.skipWhile (not . fieldEnd)
+    return $ Field (C.unpack n) (C.unpack t)
+
